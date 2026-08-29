5873d8efc41e8f7f2ba11b922c5fabf8b9592c7c
Abemelek Mesfin <abemelekmesfin07@gmail.com>
2026-08-29 23:18:24 +0300

    Last Commit

--- mobile/lib/core/constants/app_constants.dart
+++ mobile/lib/core/constants/app_constants.dart
@@ -2,5 +2,5 @@ class AppConstants {
   static const String appName = "TIMI";
 
   // Using 127.0.0.1 because we've set up ADB reverse port forwarding!
-  static const String baseUrl = "http://127.0.0.1:8000/api";
+  static const String baseUrl = "https://timi-api-9kbw.onrender.com/api";
 }
